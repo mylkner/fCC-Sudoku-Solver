b65de53ece84a5802690bf8bcca8cd321c6795e5
mylkner <mylofaulkner27@gmail.com>
2024-04-14 10:09:24 +0200

    added tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,100 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+let solver = new Solver();
+const puzzleString =
+    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+    test("Solves valid puzzle string", function () {
+        assert.strictEqual(
+            solver.validate(puzzleString),
+            "is valid",
+            "Handles valid puzzle string"
+        );
+    });
+    test("Handles puzzle string with invalid characters", function () {
+        assert.deepEqual(
+            solver.validate(
+                "a.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."
+            ),
+            { error: "Invalid characters in puzzle" },
+            "Handles invalid characters"
+        );
+    });
+    test("Handles puzzle string with invalid length", function () {
+        assert.deepEqual(
+            solver.validate("invalid_length"),
+            { error: "Expected puzzle to be 81 characters long" },
+            "Handles invalid length"
+        );
+    });
+    test("Handles valid row placement", function () {
+        assert.deepEqual(solver.check(puzzleString, 3, 2, 2), {
+            valid: false,
+            conflict: ["column", "region"],
+        }),
+            "Handles valid row placement";
+    });
+    test("Handles invalid row placement", function () {
+        assert.deepEqual(solver.check(puzzleString, 0, 6, 5), {
+            valid: false,
+            conflict: ["row"],
+        }),
+            "Handles invalid row placement";
+    });
+    test("Handles valid column placement", function () {
+        assert.deepEqual(solver.check(puzzleString, 4, 3, 6), {
+            valid: false,
+            conflict: ["region", "row"],
+        }),
+            "Handles valid column placement";
+    });
+    test("Handles invalid column placement", function () {
+        assert.deepEqual(solver.check(puzzleString, 4, 0, 5), {
+            valid: false,
+            conflict: ["column"],
+        }),
+            "Handles invalid column placement";
+    });
+    test("Handles valid region placement", function () {
+        assert.deepEqual(solver.check(puzzleString, 1, 7, 8), {
+            valid: false,
+            conflict: ["row", "column"],
+        }),
+            "Handles valid region placement";
+    });
+    test("Handles invalid region placement", function () {
+        assert.deepEqual(solver.check(puzzleString, 6, 4, 3), {
+            valid: false,
+            conflict: ["region"],
+        }),
+            "Handles invalid region placement";
+    });
+    test("Solves valid string", function () {
+        assert.strictEqual(
+            solver.solve(puzzleString),
+            "769235418851496372432178956174569283395842761628713549283657194516924837947381625",
+            "Solves valid string"
+        );
+    });
+    test("Fails to solve invalid string", function () {
+        assert.strictEqual(
+            solver.solve(
+                "111112.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."
+            ),
+            false,
+            "Fails to solve invalid string"
+        );
+    });
+    test("Solves puzzle", function () {
+        assert.strictEqual(
+            solver.solve(
+                "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."
+            ),
+            "135762984946381257728459613694517832812936745357824196473298561581673429269145378",
+            "Can solve"
+        );
+    });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,219 @@
 const chai = require("chai");
-const chaiHttp = require('chai-http');
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', () => {
+suite("Functional Tests", function () {
+    this.timeout(0);
+    const puzzle =
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
 
+    test("POST /api/solve with valid string", function (done) {
+        chai.request(server)
+            .post("/api/solve")
+            .send({ puzzle: puzzle })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                    solution:
+                        "135762984946381257728459613694517832812936745357824196473298561581673429269145378",
+                });
+                done();
+            });
+    });
+    test("POST /api/solve with missing string", function (done) {
+        chai.request(server)
+            .post("/api/solve")
+            .send({ puzzle: "" })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, { error: "Required field missing" });
+                done();
+            });
+    });
+    test("POST /api/solve with invalid character string", function (done) {
+        chai.request(server)
+            .post("/api/solve")
+            .send({ puzzle: "a" + puzzle.slice(1) })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                    error: "Invalid characters in puzzle",
+                });
+                done();
+            });
+    });
+    test("POST /api/solve with incorrect length string", function (done) {
+        chai.request(server)
+            .post("/api/solve")
+            .send({ puzzle: puzzle.slice(1, 76) })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                    error: "Expected puzzle to be 81 characters long",
+                });
+                done();
+            });
+    });
+    test("POST /api/solve with unsolvable string", function (done) {
+        chai.request(server)
+            .post("/api/solve")
+            .send({
+                puzzle: "111112.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+            })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                    error: "Puzzle cannot be solved",
+                });
+                done();
+            });
+    });
+    test("POST /api/check with all fields", function (done) {
+        chai.request(server)
+            .post("/api/check")
+            .send({
+                puzzle: puzzle,
+                coordinate: "a1",
+                value: "1",
+            })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                    valid: true,
+                });
+                done();
+            });
+    });
+    test("POST /api/check with single conflict", function (done) {
+        chai.request(server)
+            .post("/api/check")
+            .send({
+                puzzle: puzzle,
+                coordinate: "a2",
+                value: "8",
+            })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.valid, false);
+                assert.deepEqual(res.body.conflict, ["row"]);
+                done();
+            });
+    });
+    test("POST /api/check with multiple conflicts", function (done) {
+        chai.request(server)
+            .post("/api/check")
+            .send({
+                puzzle: puzzle,
+                coordinate: "a5",
+                value: "4",
+            })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.valid, false);
+                assert.deepEqual(res.body.conflict, ["row", "column"]);
+                done();
+            });
+    });
+    test("POST /api/check with all conflicts", function (done) {
+        chai.request(server)
+            .post("/api/check")
+            .send({
+                puzzle: puzzle,
+                coordinate: "a1",
+                value: "2",
+            })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.equal(res.body.valid, false);
+                assert.deepEqual(res.body.conflict, [
+                    "row",
+                    "region",
+                    "column",
+                ]);
+                done();
+            });
+    });
+    test("POST /api/check with missing fields", function (done) {
+        chai.request(server)
+            .post("/api/check")
+            .send({
+                puzzle: puzzle,
+                coordinate: "",
+                value: "2",
+            })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                    error: "Required field(s) missing",
+                });
+                done();
+            });
+    });
+    test("POST /api/check with invalid characters", function (done) {
+        chai.request(server)
+            .post("/api/check")
+            .send({
+                puzzle: "A" + puzzle.slice(1),
+                coordinate: "a1",
+                value: "2",
+            })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                    error: "Invalid characters in puzzle",
+                });
+                done();
+            });
+    });
+    test("POST /api/check with incorrect length", function (done) {
+        chai.request(server)
+            .post("/api/check")
+            .send({
+                puzzle: puzzle.slice(1),
+                coordinate: "a1",
+                value: "2",
+            })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                    error: "Expected puzzle to be 81 characters long",
+                });
+                done();
+            });
+    });
+    test("POST /api/check with invalid coordinate", function (done) {
+        chai.request(server)
+            .post("/api/check")
+            .send({
+                puzzle: puzzle,
+                coordinate: "j0",
+                value: "2",
+            })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                    error: "Invalid coordinate",
+                });
+                done();
+            });
+    });
+    test("POST /api/check with invalid value", function (done) {
+        chai.request(server)
+            .post("/api/check")
+            .send({
+                puzzle: puzzle,
+                coordinate: "a1",
+                value: "10",
+            })
+            .end(function (req, res) {
+                assert.equal(res.status, 200);
+                assert.deepEqual(res.body, {
+                    error: "Invalid value",
+                });
+                done();
+            });
+    });
 });
-
